0d8a9b30b1add6ee52e49582dc93d77dd24f5bbb
OpenFluke <openfluke@hotmail.com>
2025-04-07 15:11:12 +1000

    need this for later

---
+++ partitioning.go
@@ -0,0 +1,128 @@
+package paragon
+
+import "fmt"
+
+func (n *Network) ForwardTagged(inputs [][]float64, numTags int, selectedTag int) {
+	inputGrid := n.Layers[n.InputLayer]
+	if len(inputs) != inputGrid.Height || len(inputs[0]) != inputGrid.Width {
+		panic(fmt.Sprintf("input dimensions mismatch: expected %dx%d, got %dx%d", inputGrid.Height, inputGrid.Width, len(inputs), len(inputs[0])))
+	}
+	for y := 0; y < inputGrid.Height; y++ {
+		for x := 0; x < inputGrid.Width; x++ {
+			inputGrid.Neurons[y][x].Value = inputs[y][x]
+		}
+	}
+
+	for l := 1; l < len(n.Layers); l++ {
+		layer := n.Layers[l]
+		if l == n.OutputLayer {
+			// Compute all neurons in the output layer
+			for y := 0; y < layer.Height; y++ {
+				for x := 0; x < layer.Width; x++ {
+					neuron := layer.Neurons[y][x]
+					sum := neuron.Bias
+					for _, conn := range neuron.Inputs {
+						src := n.Layers[conn.SourceLayer].Neurons[conn.SourceY][conn.SourceX]
+						sum += src.Value * conn.Weight
+					}
+					neuron.Value = applyActivation(sum, neuron.Activation)
+					if n.Debug {
+						fmt.Printf("Layer %d, Neuron (%d,%d): Value=%.4f\n", l, x, y, neuron.Value)
+					}
+				}
+			}
+		} else {
+			// Compute only tagged section in hidden layers and zero others
+			width := layer.Width
+			startX := (width * selectedTag) / numTags
+			endX := (width * (selectedTag + 1)) / numTags
+			for y := 0; y < layer.Height; y++ {
+				for x := 0; x < layer.Width; x++ {
+					if x >= startX && x < endX {
+						neuron := layer.Neurons[y][x]
+						sum := neuron.Bias
+						for _, conn := range neuron.Inputs {
+							src := n.Layers[conn.SourceLayer].Neurons[conn.SourceY][conn.SourceX]
+							sum += src.Value * conn.Weight
+						}
+						neuron.Value = applyActivation(sum, neuron.Activation)
+					} else {
+						layer.Neurons[y][x].Value = 0 // Zero out non-tagged neurons
+					}
+				}
+			}
+		}
+	}
+
+	if n.Layers[n.OutputLayer].Neurons[0][0].Activation == "softmax" {
+		n.ApplySoftmax()
+	}
+}
+func (n *Network) BackwardTagged(targets [][]float64, learningRate float64, numTags int, selectedTag int) {
+	numLayers := len(n.Layers)
+	errorTerms := make([][][]float64, numLayers)
+	for l := range n.Layers {
+		errorTerms[l] = make([][]float64, n.Layers[l].Height)
+		for y := range errorTerms[l] {
+			errorTerms[l][y] = make([]float64, n.Layers[l].Width)
+		}
+	}
+
+	// Compute error for all output neurons
+	outputLayer := n.Layers[n.OutputLayer]
+	for y := 0; y < outputLayer.Height; y++ {
+		for x := 0; x < outputLayer.Width; x++ {
+			neuron := outputLayer.Neurons[y][x]
+			errorTerms[n.OutputLayer][y][x] = (targets[y][x] - neuron.Value) * activationDerivative(neuron.Value, neuron.Activation)
+		}
+	}
+
+	for l := n.OutputLayer; l > 0; l-- {
+		layer := n.Layers[l]
+		prev := n.Layers[l-1]
+		width := layer.Width
+		startX := (width * selectedTag) / numTags
+		endX := (width * (selectedTag + 1)) / numTags
+
+		for y := 0; y < layer.Height; y++ {
+			for x := 0; x < layer.Width; x++ {
+				if l == n.OutputLayer || (x >= startX && x < endX) { // Update all output neurons, only tagged hidden neurons
+					neuron := layer.Neurons[y][x]
+					err := errorTerms[l][y][x]
+					neuron.Bias += learningRate * err
+					for i, conn := range neuron.Inputs {
+						srcNeuron := prev.Neurons[conn.SourceY][conn.SourceX]
+						gradW := err * srcNeuron.Value
+						if gradW > 5 {
+							gradW = 5
+						} else if gradW < -5 {
+							gradW = -5
+						}
+						neuron.Inputs[i].Weight += learningRate * gradW
+						if l-1 > 0 {
+							// Only accumulate error terms for tagged section in hidden layers
+							prevWidth := prev.Width
+							prevStartX := (prevWidth * selectedTag) / numTags
+							prevEndX := (prevWidth * (selectedTag + 1)) / numTags
+							if conn.SourceX >= prevStartX && conn.SourceX < prevEndX {
+								errorTerms[l-1][conn.SourceY][conn.SourceX] += err * conn.Weight
+							}
+						}
+					}
+				}
+			}
+		}
+
+		// Apply activation derivative only to tagged section in hidden layers
+		if l-1 > 0 {
+			prevWidth := prev.Width
+			prevStartX := (prevWidth * selectedTag) / numTags
+			prevEndX := (prevWidth * (selectedTag + 1)) / numTags
+			for y := 0; y < prev.Height; y++ {
+				for x := prevStartX; x < prevEndX; x++ {
+					errorTerms[l-1][y][x] *= activationDerivative(prev.Neurons[y][x].Value, prev.Neurons[y][x].Activation)
+				}
+			}
+		}
+	}
+}
