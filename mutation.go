96a76664c5d6ae60608efabd9220d9fc1f5fd0cc
OpenFluke <openfluke@hotmail.com>
2025-05-20 20:38:46 +1000

    fixing pertu weights lol

--- mutation.go
+++ mutation.go
@@ -10,33 +10,101 @@ import (
 func (n *Network[T]) PerturbWeights(rate float64, seed int) {
 	rng := rand.New(rand.NewSource(int64(seed)))
 
-	for l := 1; l < len(n.Layers); l++ { // Skip input layer
+	for l := 1; l < len(n.Layers); l++ {
 		layer := n.Layers[l]
 		for y := 0; y < layer.Height; y++ {
 			for x := 0; x < layer.Width; x++ {
 				neuron := layer.Neurons[y][x]
 				for k := range neuron.Inputs {
-					curr := neuron.Inputs[k].Weight
+					weight := neuron.Inputs[k].Weight
 
-					switch any(curr).(type) {
+					switch any(weight).(type) {
+
 					case float32, float64:
 						noise := rng.NormFloat64() * rate
 						neuron.Inputs[k].Weight += T(noise)
 
-					case int, int8, int16, int32, int64:
+					case int:
+						v := int(weight)
 						noise := int(rng.NormFloat64() * rate * 10)
-						neuron.Inputs[k].Weight += T(noise)
+						neuron.Inputs[k].Weight = T(v + noise)
 
-					case uint, uint8, uint16, uint32, uint64:
+					case int8:
+						v := int8(weight)
+						noise := int8(rng.NormFloat64() * rate * 10)
+						neuron.Inputs[k].Weight = T(v + noise)
+
+					case int16:
+						v := int16(weight)
+						noise := int16(rng.NormFloat64() * rate * 10)
+						neuron.Inputs[k].Weight = T(v + noise)
+
+					case int32:
+						v := int32(weight)
+						noise := int32(rng.NormFloat64() * rate * 10)
+						neuron.Inputs[k].Weight = T(v + noise)
+
+					case int64:
+						v := int64(weight)
+						noise := int64(rng.NormFloat64() * rate * 10)
+						neuron.Inputs[k].Weight = T(v + noise)
+
+					case uint:
+						v := uint(weight)
 						noise := int(rng.NormFloat64() * rate * 10)
-						newVal := int(any(curr).(uint)) + noise
+						newVal := int(v) + noise
 						if newVal < 0 {
 							newVal = 0
 						}
-						neuron.Inputs[k].Weight = T(newVal)
+						neuron.Inputs[k].Weight = T(uint(newVal))
+
+					case uint8:
+						v := uint8(weight)
+						noise := int(rng.NormFloat64() * rate * 10)
+						newVal := int(v) + noise
+						if newVal < 0 {
+							newVal = 0
+						}
+						if newVal > 255 {
+							newVal = 255
+						}
+						neuron.Inputs[k].Weight = T(uint8(newVal))
+
+					case uint16:
+						v := uint16(weight)
+						noise := int(rng.NormFloat64() * rate * 10)
+						newVal := int(v) + noise
+						if newVal < 0 {
+							newVal = 0
+						}
+						if newVal > 65535 {
+							newVal = 65535
+						}
+						neuron.Inputs[k].Weight = T(uint16(newVal))
+
+					case uint32:
+						v := uint32(weight)
+						noise := int(rng.NormFloat64() * rate * 10)
+						newVal := int64(v) + int64(noise)
+						if newVal < 0 {
+							newVal = 0
+						}
+						if newVal > int64(^uint32(0)) {
+							newVal = int64(^uint32(0))
+						}
+						neuron.Inputs[k].Weight = T(uint32(newVal))
+
+					case uint64:
+						v := uint64(weight)
+						noise := int64(rng.NormFloat64() * rate * 10)
+						newVal := int64(v) + noise
+						if newVal < 0 {
+							newVal = 0
+						}
+						neuron.Inputs[k].Weight = T(uint64(newVal))
 
 					default:
-						fmt.Printf("⚠️ Unknown type in PerturbWeights: %T\n", curr)
+						fmt.Printf("⚠️ Unknown type in PerturbWeights: %T\n", weight)
 					}
 				}
 			}
